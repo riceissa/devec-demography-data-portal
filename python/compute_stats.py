ee05f78fc856c668adf6bbe6f435ee5abf9f0872
Vipul Naik <vipulnaik1@gmail.com>
2018-11-25 15:07:02 -0800

    Improvement to growth rate formula?

--- python/compute_stats.py
+++ python/compute_stats.py
@@ -42,7 +42,7 @@ def growth_rate(df):
         first_y = slope * first_x + intercept
         last_y = slope * last_x + intercept
 
-        growth = (last_y - first_y) / first_y
+        growth = (last_y - first_y) / (first_y * (last_x - first_x))
         result[col] = growth
 
     return result
